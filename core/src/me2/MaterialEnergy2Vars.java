491463f2ccf63a2ba6387492f8db1c2124a0adf4
TheEE145 <Arhitector2009@gmail.com>
2023-07-30 14:22:31 +0300

    item assets & other

---
+++ core/src/me2/MaterialEnergy2Vars.java
@@ -0,0 +1,8 @@
+package me2;
+
+import me13.core.logger.ILogger;
+import me13.core.logger.LoggerFactory;
+
+public class MaterialEnergy2Vars {
+    public static final ILogger LOGGER = LoggerFactory.build("MaterialEnergy2");
+}
--- src/me2/MaterialEnergy2.java
+++ src/me2/MaterialEnergy2.java
@@ -2,8 +2,11 @@ package me2;
 
 import mindustry.mod.Mod;
 
+import static me2.MaterialEnergy2Vars.*;
+
 @SuppressWarnings("unused")
 public class MaterialEnergy2 extends Mod {
     public MaterialEnergy2() {
+        LOGGER.info("Loaded constructor");
     }
 }
